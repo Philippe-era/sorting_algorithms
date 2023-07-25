ed7a5ee78e5de1b6f82cccb3d526a4ea98ff28a6
= <=>
2023-07-25 09:38:45 -0700

    Task 4 changed

--- 1-main.c
+++ 1-main.c
@@ -3,56 +3,55 @@
 #include "sort.h"
 
 /**
- * main – This is the main function contains all written code to be executed
- * Return: Always 0 when successfully run
- */
-int main(void)
-{
-	listint_t *list_elements;
-	int array_elements[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-	size_t num_elements = sizeof(array_elements) / sizeof(array_elements[0]);
-
-	list_elements = create_listint(array_elements, num_elements);
-	if (!list_elements)
-		return (1);
-	print_list(list_elements);
-	printf("\n");
-	insertion_sort_list(&list_elements);
-	printf("\n");
-	print_list(list_elements);
-	return (0);
-}
-
-/**
- * create_listint – Doubles initial list into second
+ * create_listint - Creates a doubly linked list from an array of integers
  *
- * @array: we converting it to the doubled array you know the vibe
- * @size: length of arrays in the section
+ * @array: Array to convert to a doubly linked list
+ * @size: Size of the array
  *
- * Return: Points to the actual succseful array
+ * Return: Pointer to the first element of the created list. NULL on failure
  */
 listint_t *create_listint(const int *array, size_t size)
 {
-	listint_t *list_elements;
-	listint_t *double_node;
-	int *temporary_node;
+	listint_t *list;
+	listint_t *node;
+	int *tmp;
 
-	list_elements = NULL;
+	list = NULL;
 	while (size--)
 	{
-		double_node = malloc(sizeof(*double_node));
-		if (!double_node)
+		node = malloc(sizeof(*node));
+		if (!node)
 			return (NULL);
-		temporary_node = (int *)&double_node->n;
-		*temporary_node = array[size];
-		double_node->next = list_elements;
-		double_node->prev = NULL;
-		list_elements = double_node;
-		if (list_elements->next)
-			list_elements->next->prev = list_elements;
+		tmp = (int *)&node->n;
+		*tmp = array[size];
+		node->next = list;
+		node->prev = NULL;
+		list = node;
+		if (list->next)
+			list->next->prev = list;
 	}
-	return (list_elements);
+	return (list);
 }
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	listint_t *list;
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	size_t n = sizeof(array) / sizeof(array[0]);
 
+	list = create_listint(array, n);
+	if (!list)
+		return (1);
+	print_list(list);
+	printf("\n");
+	insertion_sort_list(&list);
+	printf("\n");
+	print_list(list);
+	return (0);
+}
 
